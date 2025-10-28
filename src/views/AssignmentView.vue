11ced341c6b9acd3abe713a0a4b732884557569c
huansong1214 <huansong1214@gmail.com>
2025-10-28 10:51:52 -0400

    feat: build assignment view page

---
+++ src/views/AssignmentView.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1>Assignments</h1>
+    <ReusableTable :headers="headers" :items="items" />
+  </div>
+</template>
+
+<script>
+import ReusableTable from '@/components/ReusableTable.vue';
+
+export default {
+  name: 'AssignmentView',
+  components: { ReusableTable },
+  data() {
+    return {
+      headers: ['Task', 'Status', 'Due Date'],
+      items: [
+        { Task: 'Create Reusable Table Component', Status: 'Ready', 'Due Date': '2025-10-31' },
+        { Task: 'Build the "Assignment" View', Status: 'Ready', 'Due Date': '2025-10-31' },
+        { Task: 'Build the "Working With Us" View', Status: 'Ready', 'Due Date': '2025-10-31' },
+        { Task: 'Configure Vue Router and Navigation', Status: 'Ready', 'Due Date': '2025-10-31' },
+      ],
+    };
+  },
+};
+</script>
