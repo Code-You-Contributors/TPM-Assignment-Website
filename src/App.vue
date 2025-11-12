affa31f1b517424eba047c9e1a8281e913467961
huansong1214 <huansong1214@gmail.com>
2025-11-12 13:45:03 -0500

    refactor: move navigation menu from App.vue to NavMenu.vue component

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,10 @@
-<script setup></script>
+<script setup>
+import NavMenu from './components/NavMenu.vue';
+</script>
 
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/starters">Starters</router-link> |
-    <router-link to="/assignments">Assignments</router-link> |
-    <router-link to="/working-with-us">Working With Us</router-link>
-  </nav>
-    <router-view />
+  <NavMenu />
+  <router-view />
 </template>
 
-<style scoped>
-  nav a {
-    color: var(--black-cherry);
-    font-family: var(--font-alt-headline);
-    text-decoration: none;
-    font-weight: bold;
-    font-size: 1.1em;
-  }
-</style>
+<style scoped></style>
---
+++ src/components/NavMenu.vue
@@ -0,0 +1,25 @@
+<template>
+  <aside>
+    <nav class="nav-menu">
+      <router-link to="/">Home</router-link>
+      <router-link to="/starters">Starters</router-link>
+      <router-link to="/assignments">Assignments</router-link>
+      <router-link to="/working-with-us">Working With Us</router-link>
+    </nav>
+  </aside>
+
+</template>
+
+<style scoped>
+  aside nav {
+    display: flex;
+    flex-direction: column;
+  }
+  nav a {
+    color: var(--black-cherry);
+    font-family: var(--font-alt-headline);
+    text-decoration: none;
+    font-weight: bold;
+    font-size: 1.1em;
+  }
+</style>
