edf1653df34bb2213af124f1e89f444eafe28916
huansong1214 <huansong1214@gmail.com>
2025-11-12 23:36:24 -0500

    feat(nav): add responsive hamburger menu with media queries to match Job Board website

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,33 @@ import NavMenu from './components/NavMenu.vue';
 </script>
 
 <template>
-  <NavMenu />
-  <router-view />
+  <div class="layout">
+    <NavMenu />
+    <div class="content">
+      <router-view />
+    </div>
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.layout {
+  display: flex;
+  flex-direction: row;
+}
+
+content {
+  flex: 1;
+  padding: 40px 30px;
+  background-color: var(--bg-1);
+  color: var(--black-cherry);
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+
+@media (max-width: 960px) {
+  .layout {
+    flex-direction: column;
+  }
+}
+</style>
--- src/components/NavMenu.vue
+++ src/components/NavMenu.vue
@@ -1,25 +1,123 @@
 <template>
-  <aside>
-    <nav class="nav-menu">
-      <router-link to="/">Home</router-link>
-      <router-link to="/starters">Starters</router-link>
-      <router-link to="/assignments">Assignments</router-link>
-      <router-link to="/working-with-us">Working With Us</router-link>
+  <aside :class="isOpen ? 'active' : ''">
+    <nav>
+      <router-link to="/" v-on:click="closeMenu">Home</router-link>
+      <router-link to="/starters" v-on:click="closeMenu">Starters</router-link>
+      <router-link to="/assignments" v-on:click="closeMenu">Assignments</router-link>
+      <router-link to="/working-with-us" v-on:click="closeMenu">Working With Us</router-link>
     </nav>
   </aside>
-
+  <div class="content">
+    <div class="hamburger" v-on:click="toggleMenu">
+      <i v-bind:class="['fa-solid', isOpen ? 'fa-x' : 'fa-bars']"></i>
+    </div>
+  </div>
 </template>
 
+<script setup>
+import { ref } from 'vue'
+
+const isOpen = ref(false)
+
+function toggleMenu() {
+  isOpen.value = !isOpen.value
+}
+
+function closeMenu() {
+  isOpen.value = false
+}
+</script>
+
 <style scoped>
+aside {
+  background-color: var(--bg-5);
+  color: var(--black-cherry);
+  font-family: var(--font-alt-headline);
+  padding: 40px 20px;
+  width: 200px;
+  flex-shrink: 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-right: 20px;
+}
+
+aside a {
+  display: flex;
+  align-items: center;
+  font-family: var(--font-alt-headline);
+  text-decoration: none;
+  color: var(--blue);
+  gap: 10px;
+  margin-bottom: 10px;
+  font-weight: bold;
+  font-size: 1.1em;
+  transition: color 0.3s ease;
+}
+
+.hamburger {
+  font-size: 1rem;
+  position: fixed;
+  top: 10px;
+  right: 10px;
+  display: none;
+  width: 2rem;
+  height: 2rem;
+  background-color: var(--black-cherry);
+  color: var(--bg-5);
+  justify-content: center;
+  align-items: center;
+  z-index: 10;
+  border-radius: 50%;
+  cursor: pointer;
+}
+
+@media (max-width: 960px) {
+  aside {
+    height: 50px;
+    width: 100vw;
+    padding: 40px 20px;
+    flex-direction: row;
+  }
+
   aside nav {
     display: flex;
+    justify-content: space-around;
+    align-items: center;
+    flex-basis: 75%;
+  }
+
+  aside nav a {
+    margin-bottom: 0;
+    font-size: 1.25rem;
+  }
+}
+
+@media (max-width: 768px) {
+  aside {
+    width: 100vw;
+    flex-direction: column;
+    z-index: 2;
+    height: 100vh;
+    position: fixed;
+    top: -100vh;
+    transition: all 0.2s;
+  }
+
+  aside.active {
+    top: 0;
+  }
+
+  aside nav {
     flex-direction: column;
   }
-  nav a {
-    color: var(--black-cherry);
-    font-family: var(--font-alt-headline);
-    text-decoration: none;
-    font-weight: bold;
-    font-size: 1.1em;
+
+  aside nav a {
+    font-size: 2rem;
   }
+
+  .hamburger {
+    display: flex;
+  }
+}
 </style>
