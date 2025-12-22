0b162ca8e066282d04b3a9d98e22d523fc92b958
huansong1214 <huansong1214@gmail.com>
2025-12-22 15:11:02 -0500

    feat: add static projects data

---
+++ src/data/projects.js
@@ -0,0 +1,20 @@
+export const projects = [
+  {
+    id: 1,
+    pathway: 'data',
+    repoLink: 'https://github.com/example/data-project',
+    description: 'This is a data project'
+  },
+  {
+    id: 2,
+    pathway: 'webDev',
+    repoLink: 'https://github.com/example/web-project',
+    description: 'This is a web development project'
+  },
+  {
+    id: 3,
+    pathway: 'softwareDev',
+    repoLink: 'https://github.com/example/software-project',
+    description: 'This is a software development project'
+  }
+]
