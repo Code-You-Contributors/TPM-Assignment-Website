0d9c79792209bd04d89ff2dfce11a0c38e0a62f0
huansong1214 <huansong1214@gmail.com>
2025-12-22 15:50:39 -0500

    feat: render static projects by pathway

--- src/views/ContinuedLearning.vue
+++ src/views/ContinuedLearning.vue
@@ -6,14 +6,64 @@
   <div class="container">
     <section>
       <h2>Data Analysis</h2>
+
+      <div
+        v-for="project in dataProjects"
+        :key="project.id"
+        class="project"
+      >
+        <a :href="project.repoLink" target="_blank" rel="noopener">
+          {{ project.repoLink.replace('https://', '') }}
+        </a>
+        <p>{{ project.description }}</p>
+      </div>
     </section>
 
     <section>
       <h2>Web Development</h2>
+
+      <div
+        v-for="project in webDevProjects"
+        :key="project.id"
+        class="project"
+      >
+        <a :href="project.repoLink" target="_blank" rel="noopener">
+          {{ project.repoLink.replace('https://', '') }}
+        </a>
+        <p>{{ project.description }}</p>
+      </div>
     </section>
 
     <section>
       <h2>Software Development</h2>
+
+      <div
+        v-for="project in softwareProjects"
+        :key="project.id"
+        class="project"
+      >
+        <a :href="project.repoLink" target="_blank" rel="noopener">
+          {{ project.repoLink.replace('https://', '') }}
+        </a>
+        <p>{{ project.description }}</p>
+      </div>
     </section>
   </div>
 </template>
+
+<script setup>
+import { computed } from 'vue';
+import { projects } from '@/data/projects';
+
+const dataProjects = computed(() => projects.filter(p => p.pathway === 'data'))
+
+const webDevProjects = computed(() => projects.filter(p => p.pathway === 'webDev'))
+
+const softwareProjects = computed(() => projects.filter(p => p.pathway === 'softwareDev'))
+</script>
+
+<style scoped>
+  .project {
+    border-bottom: 1px solid var(--black-berry);
+  }
+</style>
